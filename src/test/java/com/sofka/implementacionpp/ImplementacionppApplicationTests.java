133d09fce26bc7c1acc958bc8fbb130a12cd5df5
Camilo U <cauribeva@unal.edu.co>
2021-09-27 15:13:15 -0500

    Started package creation

---
+++ src/test/java/com/sofka/implementacionpp/ImplementacionppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.implementacionpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementacionppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
